d90b154a8a10e59fe6287b59e237622eb2b18f85
root <root@pve-vm.fritz.box>
2026-07-14 20:57:16 +0200

    chore(firecrawl): point ct source at fork branch for dev testing

--- ct/firecrawl.sh
+++ ct/firecrawl.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/esatbayhan/proxmox-community-scripts/firecrawl-script/misc/build.func)
+# TODO: revert to upstream before PR merge:
+# source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 # Copyright (c) 2021-2026 community-scripts ORG
 # Author: esatbayhan
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
